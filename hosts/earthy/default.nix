489dc6e271acd99c43f9c57a7749b41870740f6f
willow <42willow@pm.me>
2024-08-14 15:16:09 +1000

    feat: mount arch partition on boot

--- hosts/earthy/default.nix
+++ hosts/earthy/default.nix
@@ -12,6 +12,11 @@
     ./../../modules/core
   ];
 
+  fileSystems."/run/media/arch" = {
+    device = "/dev/disk/by-uuid/17a0b73e-30de-424f-9f3c-44294696695a";
+    fsType = "btrfs";
+  };
+
   # Bootloader.
   boot.loader = {
     systemd-boot = {
