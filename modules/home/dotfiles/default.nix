4fadca5334d810a583478bd33e989ab8729fe955
willow <42willow@pm.me>
2024-08-15 20:04:52 +1000

    feat(hm): init ags config

--- modules/home/dotfiles/default.nix
+++ modules/home/dotfiles/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   home.file = {
     ".config/hypr".source = ./hypr;
+    ".config/ags".source = ./ags;
     # ".config/tofi".source = dotfiles/tofi;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
