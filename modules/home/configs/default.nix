75e972313c3c587dce9790f86b9a4898f500421e
willow <42willow@pm.me>
2024-08-15 18:17:03 +1000

    refactor: move stuff

---
+++ modules/home/configs/default.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  lib,
+  catppuccin,
+  ...
+}: {
+  imports = [
+    ./spicetify.nix
+    ./nushell.nix
+    ./ags.nix
+    ./packages.nix
+  ];
+
+  catppuccin = {
+    flavor = "macchiato";
+    accent = "pink";
+    enable = true;
+    pointerCursor = {
+      enable = true;
+      flavor = "macchiato";
+      accent = "dark";
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      confirm_os_window_close = 0;
+      cursor_blink_interval = 0;
+      mouse_hide_wait = 0;
+    };
+  };
+
+  programs.bat = {enable = true;};
+
+  programs.tofi = {
+    enable = true;
+    settings = {
+      width = "100%";
+      height = "100%";
+      border-width = 0;
+      outline-width = 0;
+      padding-top = "30%";
+      padding-bottom = "30%";
+      padding-left = "8%";
+      padding-right = "8%";
+      result-spacing = 25;
+      num-results = 5;
+      font = "monospace";
+      clip-to-padding = true;
+      hide-cursor = true;
+    };
+  };
+
+  programs.btop = {enable = true;};
+
+  programs.bun = {enable = true;};
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.activation = {
+    # https://github.com/philj56/tofi/issues/115#issuecomment-1701748297
+    regenerateTofiCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      tofi_cache=${config.xdg.cacheHome}/tofi-drun
+      [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
+    '';
+  };
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+}
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,72 +1,11 @@
+{ ... }:
 {
-  config,
-  lib,
-  catppuccin,
-  ...
-}: {
   imports = [
-    ./dotfiles
-    ./spicetify.nix
-    ./nushell.nix
-    ./ags.nix
-    ./packages.nix
+    ./configs # per application configuration
+    ./dotfiles # un-nixified dotfiles
+    # ./system # important system environment config
+    # ./packages # programs that are used, e.g. GUI apps
+    # ./services # system services, organized by display protocol
+    # ./themes # Application themeing
   ];
-
-  catppuccin = {
-    flavor = "macchiato";
-    accent = "pink";
-    enable = true;
-    pointerCursor = {
-      enable = true;
-      flavor = "macchiato";
-      accent = "dark";
-    };
-  };
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      confirm_os_window_close = 0;
-      cursor_blink_interval = 0;
-      mouse_hide_wait = 0;
-    };
-  };
-
-  programs.bat = {enable = true;};
-
-  programs.tofi = {
-    enable = true;
-    settings = {
-      width = "100%";
-      height = "100%";
-      border-width = 0;
-      outline-width = 0;
-      padding-top = "30%";
-      padding-bottom = "30%";
-      padding-left = "8%";
-      padding-right = "8%";
-      result-spacing = 25;
-      num-results = 5;
-      font = "monospace";
-      clip-to-padding = true;
-      hide-cursor = true;
-    };
-  };
-
-  programs.btop = {enable = true;};
-
-  programs.bun = {enable = true;};
-
-  nixpkgs.config.allowUnfree = true;
-
-  home.activation = {
-    # https://github.com/philj56/tofi/issues/115#issuecomment-1701748297
-    regenerateTofiCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      tofi_cache=${config.xdg.cacheHome}/tofi-drun
-      [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
-    '';
-  };
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
 }
