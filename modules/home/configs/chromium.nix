6dfa13998e24d174bad3d83c0a46971ed2558931
willow <42willow@pm.me>
2024-08-28 17:06:11 +1000

    feat: chromium

---
+++ modules/home/configs/chromium.nix
@@ -0,0 +1,14 @@
+{...}:
+{
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+      { id = "denkadefchjkchlefgfhdmabagdcefhf"; } # schooltape
+      { id = "gebbhagfogifgggkldgodflihgfeippi"; } # return youtube dislike
+      { id = "cmpdlhmnmjhihmcfnigoememnffkimlk"; } # catppuccin macchiato theme
+      { id = "lnjaiaapbakfhlbjenjkhffcdpoompki"; } # catppuccin for github file explorer icons
+      { id = "oboonakemofpalcgghocfoadofidjkkk"; } # keepassxc
+    ];
+  };
+}
--- modules/home/configs/default.nix
+++ modules/home/configs/default.nix
@@ -14,6 +14,7 @@
     ./git.nix
     ./services.nix
     ./xdg.nix
+    ./chromium.nix
   ];
 
   catppuccin = {
