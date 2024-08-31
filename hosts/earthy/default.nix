a9591580391e10076e47161cb21455ae650166d6
willow <42willow@pm.me>
2024-08-31 16:11:54 +1000

    feat: tlp

--- hosts/earthy/default.nix
+++ hosts/earthy/default.nix
@@ -17,4 +17,14 @@
       vaapiIntel
     ];
   };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      PLATFORM_PROFILE_ON_AC="performance";
+      PLATFORM_PROFILE_ON_BAT="low-power";
+    };
+  };
 }
