b21e06d28d6c2f6301835352b6f9681ebe6b3871
willow <42willow@pm.me>
2024-09-01 18:03:29 +1000

    feat: update and steam

--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -3,16 +3,17 @@
   inputs,
   ...
 }: {
-  programs.hyprland = {
-    enable = true;
-    # package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+  programs = {
+    hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    };
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+    };
+    steam.enable = true;
+    localsend.enable = true;
   };
-
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-  };
-
-  programs.localsend.enable = true;
 }
