1e6c98bcd77027acd65d14cb66390f8a045f4d60
willow <42willow@pm.me>
2024-09-03 18:39:11 +1000

    feat: avahi printing

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -2,7 +2,15 @@
   services = {
     gvfs.enable = true;
     gnome.gnome-keyring.enable = true;
+
+    # Printing
     printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
     # dbus.enable = true;
     # fstrim.enable = true;
     xserver.xkb = {
