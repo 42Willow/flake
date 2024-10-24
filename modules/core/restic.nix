c0bed58b3e0ea7710ebb48f8da8b0d6d6efd078e
willow <42willow@pm.me>
2024-10-24 17:31:57 +1100

    feat: update restic config

--- modules/core/restic.nix
+++ modules/core/restic.nix
@@ -1,21 +1,36 @@
-{...}: {
+{pkgs, ...}: {
+  users.users.restic = {
+    isNormalUser = true;
+  };
+
+  security.wrappers.restic = {
+    source = "${pkgs.restic.out}/bin/restic";
+    owner = "restic";
+    group = "users";
+    permissions = "u=rwx,g=,o=";
+    capabilities = "cap_dac_read_search=+ep";
+  };
+
   services.restic.backups = {
     remotebackup = {
-      user = "willow";
       passwordFile = "/etc/nixos/secrets/restic-password";
       paths = [
         "/home/willow/docs"
         "/home/willow/git"
+        "/home/willow/media"
       ];
       repository = "/mnt/share/restic";
       timerConfig = {
-        OnCalendar = "13:00";
+        OnCalendar = "daily";
         Persistent = true;
       };
       pruneOpts = [
         "--keep-daily 7"
         "--keep-weekly 5"
         "--keep-monthly 12"
+      ];
+      extraBackupArgs = [
+        "--exclude=\"node_modules/*\""
       ];
     };
   };
