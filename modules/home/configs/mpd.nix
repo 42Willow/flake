906ca817fcc05758800d22c1d8e112469502ad10
willow <42willow@pm.me>
2024-10-31 20:54:49 +1100

    feat(mpd): visualiser, rpc, and styling

--- modules/home/configs/mpd.nix
+++ modules/home/configs/mpd.nix
@@ -10,6 +10,12 @@
         type "pipewire"
         name "PipeWire"
       }
+      audio_output {
+        type "fifo"
+        name "Visualiser"
+        path "/tmp/mpd.fifo"
+        format "44100:16:2"
+      }
     '';
   };
   programs.ncmpcpp = {
@@ -18,6 +24,68 @@
       visualizerSupport = true;
       clockSupport = true;
       taglibSupport = true;
+    };
+    # adapted for colemak
+    bindings = [
+      # navigation
+      { key = "n"; command = "scroll_down"; }
+      { key = "e"; command = "scroll_up"; }
+      { key = "N"; command = [ "select_item" "scroll_down" ]; }
+      { key = "E"; command = [ "select_item" "scroll_up" ]; }
+      # move items
+      { key = "k"; command = "move_sort_order_down"; }
+      { key = "k"; command = "move_selected_items_down"; }
+      { key = "m"; command = "move_sort_order_up"; }
+      { key = "m"; command = "move_selected_items_up"; }
+    ];
+    settings = {
+      # visualiser
+      visualizer_data_source = "/tmp/mpd.fifo";
+      visualizer_output_name = "mpd_visualizer";
+      visualizer_type = "ellipse";
+      visualizer_look = "●●";
+
+      # current item
+      current_item_prefix = "$(magenta)$r"; # change accent color
+
+      # selected items
+      selected_item_prefix = "\"* \"";
+      discard_colors_if_item_is_selected = "yes";
+
+      # now playing
+      now_playing_prefix = "\"$b$6  \"";
+      now_playing_suffix = "\"$/b\"";
+      centered_cursor = true;
+      autocenter_mode = "yes";
+
+      # layout
+      user_interface = "alternative";
+      startup_screen = "playlist";
+      startup_slave_screen = "visualizer";
+      song_columns_list_format = "(10)[blue]{l} (45)[magenta]{t} (15)[white]{a} (30)[white]{b}";
+
+      # display modes
+      playlist_display_mode = "columns";
+      browser_display_mode = "columns";
+      search_engine_display_mode = "columns";
+      playlist_editor_display_mode = "columns";
+
+      # colours
+      main_window_color = "white";
+
+      # navigation
+      lines_scrolled = 1; # default is 5
+    };
+  };
+  services.mpd-discord-rpc = {
+    enable = true;
+    settings = {
+      format = {
+        details = "$title";
+        state = "$artist";
+        large_text = "$album";
+        small_text = "Listening to $genre music with MPD ^-^";
+      };
     };
   };
 }
